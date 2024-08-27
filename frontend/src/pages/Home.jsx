42c9544fa3d681af6957ef5175c63dccfd24b82d
Navindu Virajitha <navinduvirajitha2001@gmail.com>
2024-08-27 11:00:45 +0530

    fixed bugs

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import Hero from "../components/Hero/Hero";
-import MessageForm from "../components/MessageForm/MessageForm";
-// import Gallery from "../components/Gallery/Gallery"
-import Footer from "../components/Footer/Footer"
+
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <MessageForm />
-      <Footer />
     </>
   );
 };
